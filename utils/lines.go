cebdc398e0e10e3db086eff9f1ab9112fa533da7
Sliverwing <tlv591208510@gmail.com>
2017-09-18 11:39:46 +0800

    add helper DailSearchLine

--- utils/lines.go
+++ utils/lines.go
@@ -17,3 +17,13 @@ func DailLineDetail(id int) (*models.LineHttpResponce, error) {
 	resp.JSON(&line)
 	return line, nil
 }
+
+func DailSearchLine(param string) (*models.LineSearchHttpResponse, error) {
+	line := &models.LineSearchHttpResponse{}
+	resp, err := grequests.Get(fmt.Sprintf("http://60.210.101.86:8980/server-ue2/rest/buslines/simple/370300/%s", param), GetOptions())
+	if err != nil {
+		return nil, err
+	}
+	resp.JSON(&line)
+	return line, nil
+}
