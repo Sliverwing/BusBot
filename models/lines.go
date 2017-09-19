efcf10b3e15554fe480076573041e8efa8547717
Sliverwing <tlv591208510@gmail.com>
2017-09-19 21:56:27 +0800

    add CachedLineStruct

--- models/lines.go
+++ models/lines.go
@@ -1,5 +1,9 @@
 package models
 
+import "errors"
+
+var CachedLine *CachedLineStruct
+
 type LineHttpResponce struct {
 	Status Status     `json:"status"`
 	Result LineResult `json:"result"`
@@ -29,4 +33,34 @@ type Station struct {
 	State       string  `json:"state"`
 	UpdateTime  string  `json:"updateTime"`
 	// busLineList ignored
+}
+
+type CachedLineStruct struct {
+	stations map[int]*map[int]Station
+}
+
+func (cl *CachedLineStruct) Init() {
+	cl.stations = map[int]*map[int]Station{}
+}
+
+func (cl *CachedLineStruct) Push(id int, stations *map[int]Station) {
+	cl.stations[id] = stations
+}
+
+func (cl *CachedLineStruct) IsExists(id int) bool {
+	for k := range cl.stations {
+		if k == id {
+			return true
+		}
+	}
+	return false
+}
+
+func (cl *CachedLineStruct) Get(id int) (*map[int]Station, error) {
+	for k, v := range cl.stations {
+		if k == id {
+			return v, nil
+		}
+	}
+	return nil, errors.New("Object Now Exists")
 }
