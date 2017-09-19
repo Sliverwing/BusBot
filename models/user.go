6cd8dbb6790b0c2052157211250fe99c07975888
Sliverwing <tlv591208510@gmail.com>
2017-09-19 09:58:36 +0800

    🤦🏻‍♂️ bug fixed related to pointer reference

--- models/user.go
+++ models/user.go
@@ -9,23 +9,27 @@ type UserStatus struct {
 	selectedLine int
 }
 
-func (u UserStatus) SetAction(action string) {
+func (u *UserStatus) SetAction(action string) {
 	u.Action = action
 }
 
-func (u UserStatus) SetCurrentPage(page int) {
+func (u *UserStatus) SetCurrentPage(page int) {
 	u.CurrentPage = page
 }
 
-func (u UserStatus) Clear() {
+func (u *UserStatus) Clear() {
 	u.Action = ""
 	u.CurrentPage = 0
 }
 
-func (u UserStatus) SelectLine(id int) {
+func (u *UserStatus) SelectLine(id int) {
 	u.selectedLine = id
 }
 
-func (u UserStatus) SelectedLine() int {
+func (u *UserStatus) SelectedLine() int {
 	return u.selectedLine
+}
+
+func (u *UserStatus) GetAction() string {
+	return u.Action
 }
